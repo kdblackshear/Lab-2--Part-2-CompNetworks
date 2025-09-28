ccf7e2bbb8a54f8c76d7423816b434aa7558e238
Kahlyn D Blackshear <kahlyn.blackshear@bison.howard.edu>
2025-09-28 21:40:06 +0000

    second commit

---
+++ signal2.c
@@ -0,0 +1,25 @@
+/* hello_signal.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int sound_alarm = 0;
+
+void handler(int signum)
+{ //signal handler
+  printf("Hello World!\n");
+  sound_alarm = 1;
+  //exit(1); //exit after printing
+}
+
+int main(int argc, char * argv[])
+{
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  while(1){
+    alarm(5); //Schedule a SIGALRM for 5 seconds
+    while(!sound_alarm); //busy wait for signal to be delivered
+    printf("Turing was right!\n");
+  }
+  return 0; //never reached
+}
